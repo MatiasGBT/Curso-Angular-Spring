9f72484b9e9aedba2488876c537fc5077478f3b2
Matías Gabriel Blanco Toledo <blancotoledomatiasgabriel@gmail.com>
2022-06-24 17:29:19 -0300

    Agrego secciones 12 y 13: Agregar la fecha en el formulario y subida de imagenes

---
+++ Sección 12 y 13 - Campo fecha y subida imagenes/clientes-app/src/app/clientes/cliente.service.ts
@@ -0,0 +1,91 @@
+import { Injectable } from '@angular/core';
+import { Cliente } from './cliente';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { HttpClient, HttpEvent, HttpHeaders, HttpRequest } from '@angular/common/http';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+  private urlEndPoint: string = 'http://localhost:8080/api/clientes';
+  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+
+  constructor(private http: HttpClient, private router:Router) { }
+
+  /* Ya que, con el paginado, la respuesta devuelve un json más completo, hay que transformar
+  sí o sí el objeto */
+  getClientes(page: number): Observable<any> {
+    return this.http.get<Cliente[]>(this.urlEndPoint + `/page/${page}`).pipe(
+      map((response:any) => {
+        response.content as Cliente[];
+        return response;
+      })
+    );
+  }
+
+  create(cliente: Cliente): Observable<any> {
+    return this.http.post<any>(this.urlEndPoint, cliente, {headers: this.httpHeaders}).pipe(
+      catchError(e => {
+
+        if (e.status==400) {
+          return throwError(()=>e);
+        }
+
+        console.error(e.error.mensaje);
+        Swal.fire(e.error.mensaje, e.error.error, 'error');
+        return throwError(()=>e);
+      })
+    );
+  }
+
+  getCliente(id: number): Observable<Cliente> {
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+      catchError(e => {
+        this.router.navigate(['/clientes']);
+        console.error(e.error.mensaje);
+        Swal.fire('Error al editar', e.error.mensaje, 'error');
+        return throwError(()=>e);
+      })
+    );
+  }
+
+  update(cliente: Cliente): Observable<any> {
+    return this.http.put<any>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders}).pipe(
+      catchError(e => {
+        
+        if (e.status==400) {
+          return throwError(()=>e);
+        }
+
+        console.error(e.error.mensaje);
+        Swal.fire(e.error.mensaje, e.error.error, 'error');
+        return throwError(()=>e);
+      })
+    );
+  }
+
+  delete(id: number): Observable<Cliente> {
+    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {headers: this.httpHeaders}).pipe(
+      catchError(e => {
+        console.error(e.error.mensaje);
+        Swal.fire(e.error.mensaje, e.error.error, 'error');
+        return throwError(()=>e);
+      })
+    );
+  }
+
+  subirFoto(archivo: File, id):Observable<HttpEvent<{}>> {
+    let formData = new FormData();
+    formData.append("archivo", archivo);
+    formData.append("id", id);
+
+    const req = new HttpRequest('POST', `${this.urlEndPoint}/upload`, formData, {
+      reportProgress: true
+    });
+
+    return this.http.request(req);
+  }
+}
---
+++ Sección 12 y 13 - Campo fecha y subida imagenes/spring/src/main/java/com/mgbt/springboot/backend/apirest/springbootbackendapirest/service/ClienteService.java
@@ -0,0 +1,47 @@
+package com.mgbt.springboot.backend.apirest.springbootbackendapirest.service;
+
+import com.mgbt.springboot.backend.apirest.springbootbackendapirest.entity.Cliente;
+import com.mgbt.springboot.backend.apirest.springbootbackendapirest.repository.IClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ClienteService implements IClienteService {
+
+    @Autowired
+    private IClienteRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<Cliente> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<Cliente> findAll(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    @Override
+    @Transactional(readOnly = false)
+    public Cliente save(Cliente cliente) {
+        return repository.save(cliente);
+    }
+
+    @Override
+    @Transactional(readOnly = false)
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Cliente findById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+}
